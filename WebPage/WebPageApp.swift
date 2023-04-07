aeec21f4062c4cea0d38c18c3bdef5fb7802f4da
Froy <54004493+froymetal@users.noreply.github.com>
2023-04-07 12:05:33 -0500

    Initial Commit

---
+++ WebPage/WebPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebPageApp.swift
+//  WebPage
+//
+//  Created by Consultant on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WebPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
