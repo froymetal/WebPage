5d22b3cb499f8e4d55bd69dd69493457f52e7467
froymetal <froylan10@hotmail.com>
2023-04-07 16:40:25 -0500

    typo issue

--- WebPage/ContentView.swift
+++ WebPage/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
                 }
                 .navigationBarBackButtonHidden(true)
                 NavigationLink(destination: BottomSheetView()) {
-                    Text("BottomSheetView Mode")
+                    Text("BottomSheet Mode")
                         .font(.headline)
                         .foregroundColor(.white)
                         .padding()
